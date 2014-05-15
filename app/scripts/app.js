1955e9092983758390a6583500872abc385665ce
Willson Mock <willson.mock@gmail.com>
2014-05-15 14:25:49 -0700

    Add routes to app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,8 +10,44 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/map.html',
+        controller: 'MapCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/gallery', {
+        templateUrl: 'views/gallery.html',
+        controller: 'GalleryCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/map', {
+        templateUrl: 'views/map.html',
+        controller: 'MapCtrl'
+      })
+      .when('/navigation', {
+        templateUrl: 'views/navigation.html',
+        controller: 'NavigationCtrl'
+      })
+      .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .when('/singlephoto', {
+        templateUrl: 'views/singlephoto.html',
+        controller: 'SinglephotoCtrl'
+      })
+      .when('/upload', {
+        templateUrl: 'views/upload.html',
+        controller: 'UploadCtrl'
       })
       .otherwise({
         redirectTo: '/'
