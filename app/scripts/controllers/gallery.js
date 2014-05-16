adf6b7cd0c3d1bcb0d873e6ce1cdfa1d2285b44a
Willson Mock <willson.mock@gmail.com>
2014-05-16 16:36:10 -0700

    Testing upload functionality with backend server that's currently set up.

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -3,4 +3,18 @@
 angular.module('wanderaweApp')
   .controller('GalleryCtrl', function ($scope) {
 
+    // array of photos from server / database that have already been filtered by criteria ('Nature in China')
+    $scope.photos = [];
+
+    // execute immediately to add functionality to each photo
+    $scope.photos.forEach(function (photo) {
+      // add on-hover functionality to photo; this includes displaying appropriate info
+
+      // add ability to like photo
+
+      // check if the current state is 'logged in'
+        // check if the current username is the username that belongs to the photo
+          // add ability to remove photo
+    });
+
   });
