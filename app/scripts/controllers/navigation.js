a2c062a4f1283ec7fe1a3b0d26ad808891b46687
Willson Mock <willson.mock@gmail.com>
2014-05-18 16:11:38 -0700

    Add ng-include in navigation.html to toggle between signup/login and profile/upload

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -3,6 +3,7 @@
 angular.module('wanderaweApp')
   .controller('NavigationCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.navigationInfo = {};
+    $scope.isSignedIn = true; // this should be false but leave true for now
 
     $scope.resetNavigationInfo = function () {
       $scope.navigationInfo = {};
