5be79bb2b496f3e2aa4508ab6076d520dd676f9b
Maestro501 <chldbwls0114@hotmail.com>
2014-05-20 17:22:30 -0700

    removed private key

--- backend/authConfig.js
+++ backend/authConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
 
   'facebook' : {
-    'clientID'    : '1448671645374053',
-    'clientSecret'  : '240d5a532e41edab837fd66a8845ae27',
+    'clientID'    : 'your-consumer-key-here',
+    'clientSecret'  : 'your-client-secret-here',
     'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
   },
 
