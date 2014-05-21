65cfc646373e4cc9e9090f210d684660d30d8a86
Willson Mock <willson.mock@gmail.com>
2014-05-21 12:23:44 -0700

    Fix uploadCtrl to include Photo, month, and year services

--- app/scripts/services/photoService.js
+++ app/scripts/services/photoService.js
@@ -6,21 +6,20 @@ angular.module('wanderaweApp')
     var gridHeight = 300; // pixels
 
     this.uploadPhoto = function (photoInfo, file) {
-      // $http.get('/auth/facebook', photoInfo)
-      // $upload
-      //   .upload({
-      //     url: 'upload',
-      //     data: photoInfo,
-      //     file: file
-      //   })
-      //   .progress(function() {
-      //     // console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-      //   })
-      //   .success(function(res) {
-      //     console.log('upload photo successfully');
-      //     $state.go('singlephoto', res);
-      //     lastUploadedFileType = res.fileType;
-      //   });
+      $upload
+        .upload({
+          url: 'upload',
+          data: photoInfo,
+          file: file
+        })
+        .progress(function() {
+          // console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+        })
+        .success(function(res) {
+          console.log('upload photo successfully');
+          $state.go('singlephoto', res);
+          lastUploadedFileType = res.fileType;
+        });
     };
 
     this.getLastUploadedFileType = function () {
