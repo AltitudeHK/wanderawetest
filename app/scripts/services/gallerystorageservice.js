6b2cc86482970085423a913f8bf524859c907210
Willson Mock <willson.mock@gmail.com>
2014-05-22 21:10:28 -0700

    Minor edit to gallerystorageservice.js

--- app/scripts/services/gallerystorageservice.js
+++ app/scripts/services/gallerystorageservice.js
@@ -2,7 +2,6 @@
 
 angular.module('wanderaweApp')
   .factory('Gallerystorageservice', function Gallerystorageservice() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
     var svc = {};
     svc.photos = [];
     return svc;
