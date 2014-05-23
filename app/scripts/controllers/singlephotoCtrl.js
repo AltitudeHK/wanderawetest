8ea34d53a2c831144f2fe0fb4b0615f8aef41f82
Willson Mock <willson.mock@gmail.com>
2014-05-22 21:13:47 -0700

    Minor edit to singlephotoCtrl.js

--- app/scripts/controllers/singlephotoCtrl.js
+++ app/scripts/controllers/singlephotoCtrl.js
@@ -5,7 +5,7 @@ angular.module('wanderaweApp')
     Photo
       .retrieveOnePhoto($stateParams.photoId)
       .success(function (data) {
-        // data looks like: { "photoId": "537acf591ba61f0000f8401d", "fileType": "jpeg", "height": 300, "width": 300 }
+        // data: { "photoId": "537acf591ba61f0000f8401d", "fileType": "jpeg", "height": 300, "width": 300 }
         $scope.photoPath = data.photoId + '.' + data.fileType;
       });
   }]);
