eefb1ae36b3454bf4446e61b6baa2923a53a45e9
Willson Mock <willson.mock@gmail.com>
2014-05-22 22:05:15 -0700

    Modify singlephotoCtrl.js to get a single photo to display correctly

--- app/scripts/controllers/singlephotoCtrl.js
+++ app/scripts/controllers/singlephotoCtrl.js
@@ -3,5 +3,6 @@
 angular.module('wanderaweApp')
   .controller('SinglephotoCtrl', ['$scope', 'picture', function ($scope, picture) {
     // picture: { "photoId": "537acf591ba61f0000f8401d", "fileType": "jpeg", "height": 300, "width": 300 }
-    $scope.photoPath = picture.photoId + '.' + picture.fileType;
+    // console.log('picture is', picture);
+    $scope.photoPath = picture.data._id + '.' + picture.data.fileType;
   }]);
